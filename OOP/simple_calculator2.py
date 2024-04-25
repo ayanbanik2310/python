23ed701fb9e4b09d09e1c5117f57bea38b4ecb12
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-04-26 01:57:59 +0600

    Create simple_calculator2.py

---
+++ OOP/simple_calculator2.py
@@ -0,0 +1,42 @@
+class calculator:
+    brand = 'casia'
+    
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def addision(self):
+        return mycalcu.num1+mycalcu.num2
+    def subtraction(self):
+        return mycalcu.num1 - mycalcu.num2
+    def multiplication(self):
+        return mycalcu.num1* mycalcu.num2
+    def division(self):
+        return mycalcu.num1 / mycalcu.num2
+
+
+if __name__ == '__main__':
+    num1 = int(input('enter number 1 : '))
+    num2 = int(input('enter number 2 : '))
+    print('option 1 : addition')
+    print('option 2 : multiplication')
+    print('option 3 : substraction')
+    print('option 4 : division')
+    n = int(input('choose and option : '))
+
+    mycalcu = calculator(num1, num2)
+    if n==1:
+        ans = mycalcu.addision()
+        print('your ans is : ', ans)
+    elif n==2:
+        ans = mycalcu.multiplication()
+        print('your ans is : ', ans)
+    elif n==3:
+        ans = mycalcu.subtraction()
+        print('your ans is : ', ans)
+    elif n==4:
+        ans = mycalcu.division()
+        print('your ans is : ', ans)
+    else:
+        print('you entered wrong option, khatam geya bye bye')
+    
